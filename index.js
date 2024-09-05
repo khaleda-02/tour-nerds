ef01bbca3961af26e963cf6f111d57c4ca2b731a
khaleda-02 <khaleda.02f@gmail.com>
2024-09-05 21:55:00 +0300

    add security to the project: req limiter, sec headers, query injection problem ...

--- index.js
+++ index.js
@@ -4,10 +4,42 @@ require("dotenv").config();
 const app = express();
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middlewares/errorMiddleware");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 
+// Db connect
 connectDb();
+
+// set security http headers
+app.use(helmet());
+
+// enable requests limiter
+app.use(
+  "/api",
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 100,
+    message: "too many requests, try after hour",
+  }),
+);
+
+// body parser
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
+
+// Data Sanitization => secure request body against query injection
+app.use(mongoSanitize());
+
+// clean the params objs, prevent params pollution
+app.use(
+  hpp({
+    whitelist: ["duration"],
+  }),
+);
+
+// cors
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
@@ -17,6 +49,7 @@ app.use("/api/tour", require("./routes/tourRoutes"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use(errorHandler);
+
 // Not found reqs
 app.all("*", (req, res, next) => {
   res.status(404).json({
